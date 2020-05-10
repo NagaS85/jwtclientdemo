cdc846dee8e2cc2410a5729a1f15ec96bcb044c3
Naga <naga048.java@gmail.com>
2020-05-10 16:12:16 +0530

    intial commit

---
+++ src/main/java/com/example/jwtdemo/JwtdemoClinetApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwtdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtdemoClinetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtdemoClinetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jwtdemo/JwtdemoClinetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwtdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtdemoClinetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
